dd5f193f42dff0e18a13b404395fd6fd27d1f84c
zemzemabdelkarim <81090104+zemzemabdelkarim@users.noreply.github.com>
2024-09-24 16:49:11 +0100

    feat! : Create operate function
    The operate function is the caller of the four function

--- src.js
+++ src.js
@@ -13,3 +13,28 @@ const multiply = function(x, y){
 const divide = function(x, y){
     return x / y;
 }
+const operate = function(x, op, y){
+    switch (op) {
+        case "+":
+            return add(x, y);
+    
+        case "-":
+            return substract(x, y);
+
+        case "*":
+            return multiply(x, y);
+
+        case "/":
+            return divide(x, y);
+
+        default:
+            console.log("Error, unknowen operator");
+            
+            break;
+    }
+}
+
+
+let firstNumber;
+let operator;
+let secondNumber;
